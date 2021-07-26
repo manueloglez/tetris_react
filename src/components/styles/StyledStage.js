fbd03c5ec0ce06bca7cb0eb207eb436ba8205f13
manueloglez <mang.95@gmail.com>
2021-07-25 17:14:23 -0700

    hooks usePlayer useStage added

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -5,12 +5,12 @@ export const StyledStage = styled.div`
   display: grid;
   grid-template-rows: repeat(
     ${props => props.height},
-    calc(50vw / ${props => props.width})
+    calc(25vw / ${props => props.width})
   );
-  grid-template-columns: repeat(${props => props.width}, calc(48.9vw / ${props => props.width}));
+  grid-template-columns: repeat(${props => props.width}, calc(24.5vw/ ${props => props.width}));
   grid-gap: 1px;
   border: 2px solid #333;
   width: 100%;
-  max-width: 50vw;
+  max-width: 25vw;
   background: #111;
 `;
